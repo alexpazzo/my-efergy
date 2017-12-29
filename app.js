100f895fd572c3da58a24a0532b315edece8266a
Unknown <alessandro.belli89@gmail.com>
2017-12-29 12:15:23 +0100

    added cron restart every 12 hours

--- app.js
+++ app.js
@@ -19,40 +19,53 @@ if ((!process.env.USERNAME || process.env.USERNAME == "") || (!process.env.PASSW
     password = process.env.PASSWORD;
 }
 
+/**
+ * Connection to MQTT
+ */
 const client = mqtt.connect(`mqtt://${server}`);
-
 //Callback on connection
 client.on('connect', setup);
 
+
+let browser;
 // fired when the mqtt server is ready
 function setup() {
     console.log(`'Connected to the mqtt://${server} server`);
-    //Login to the page
-    (async() => {
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
-        const page = await browser.newPage();
-        console.log(`https://engage.efergy.com/user/login?username=${username}&password=${password}`);
-        await page.goto(`https://engage.efergy.com/user/login?username=${username}&password=${password}`);
-
-        page.on('response', async response => {
-            if (response.url.match(/getCurrentValuesSummary/)) {
-                let data = await response.json();
-                console.log(`${new Date().getTime()}| `, data);
-                var message = {
-                    topic: 'efergy/currentValues/',
-                    payload: JSON.stringify(data), // or a Buffer
-                    qos: 0, // 0, 1, or 2
-                    retain: false // or true
-                };
-                client.publish(message.topic, message.payload, message, () => {
-                    console.log('Message Sent!');
-                })
-
-            }
-
-        })
-        //await browser.close();
-    })();
+    getEfergy();
 }
+
+let getEfergy = (async() => {
+    browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    console.log(`https://engage.efergy.com/user/login?username=${username}&password=${password}`);
+    await page.goto(`https://engage.efergy.com/user/login?username=${username}&password=${password}`);
+
+    page.on('response', async response => {
+        if (response.url.match(/getCurrentValuesSummary/)) {
+            let data = await response.json();
+            var message = {
+                topic: 'efergy/currentValues/',
+                payload: JSON.stringify(data), // or a Buffer
+                qos: 0, // 0, 1, or 2
+                retain: false // or true
+            };
+            client.publish(message.topic, message.payload);
+
+        }
+
+    })
+})
+
+const CronJob = require('cron').CronJob;
+const job = new CronJob('0 */12 * * *', function() {
+        console.log("Restarting Puppeteer");
+        if (browser) browser.close();
+        if (getEfergy) getEfergy();
+    }, function() {
+
+    },
+    true,
+    "Europe/Rome"
+);
